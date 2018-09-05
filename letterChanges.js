a7510c8c792bc74882de8e7fd6ce6ec8a1cb019d
Teresa <teresa.nesteby@gmail.com>
2018-09-04 22:02:28 -0700

    adds letterChanges

---
+++ letterChanges.js
@@ -0,0 +1,20 @@
+// Have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
+
+function LetterChanges(str) {
+ var i;
+ var stringOfNumbers = "";
+
+  for (i = 0; i < str.length; i++) {
+      var ascii= (str.charCodeAt(i)) + 1
+      var newAscii = String.fromCharCode(ascii);
+      console.log(newAscii)
+      stringOfNumbers+=newAscii
+  }
+  return stringOfNumbers;
+}
+
+console.log(LetterChanges("345"))
+
+
+
+
